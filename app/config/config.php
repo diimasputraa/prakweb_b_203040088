0c5b4f6188c0f0a33d66723cbede13b6c211e51c
diimasputraa <203040088@mail.unpas.ac.id>
2022-10-12 20:21:20 +0700

    mvc

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_b_203040088/mvc/public');
+
+//Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb_b_203040088');
---
+++ app/models/UserModel.php
@@ -0,0 +1,11 @@
+<?php
+
+class UserModel
+{
+    private $nama = 'Dimas Putra Kusumah 203040088';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
