9c6dac7fb6c05fa8d736cd7e2e7e0afd1620052e
Dimas Putra Kusumah <114494040+diimasputraa@users.noreply.github.com>
2022-10-12 20:29:01 +0700

    Delete app directory

--- app/views/about/index.php
+++
@@ -1,6 +0,0 @@
-<div class="container">
-    <h1 class='mt-4'>About Me</h1>
-    <img src="<?= BASEURL ?>/img/dim.jpg" alt="Dimas Putra Kusumah" width="200" class="rounded-circle shadow">
-    <p>Hola, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
-
-</div>
